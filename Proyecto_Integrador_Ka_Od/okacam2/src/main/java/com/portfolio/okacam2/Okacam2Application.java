f67f3c7102acffdb61ed5447fff7a2523c493651
Karen Odasso <106004749+ailen347@users.noreply.github.com>
2022-11-30 21:52:12 -0300

    Actulizacion

---
+++ Proyecto_Integrador_Ka_Od/okacam2/src/main/java/com/portfolio/okacam2/Okacam2Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.okacam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Okacam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Okacam2Application.class, args);
+	}
+
+}
